e6b465d9a7ca521b90ecadeae94a8c04d1a145cf
dilvika <cao.d.vi@gmail.com>
2021-05-20 20:23:03 +0700

    appbar

--- src/view/pages/UI-Test.js
+++ src/view/pages/UI-Test.js
@@ -7,7 +7,7 @@ const UITest = (props) => {
   return (
     <main className="dark:bg-gray-800 bg-white relative overflow-hidden h-screen">
       <GeneralAppBar></GeneralAppBar>
-      <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center">
+      {/* <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center">
         <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-8">
           <div className="flex flex-col">
             <h1 className="font-light w-full uppercase text-center text-4xl sm:text-5xl dark:text-white text-gray-800">
@@ -39,7 +39,7 @@ const UITest = (props) => {
             />
           </div>
         </div>
-      </div>
+      </div> */}
     </main>
   );
 };
