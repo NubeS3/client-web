3664e37454049be6346ee0e8b7fc5dfb7b0b0dba
Tuong Nguyen <tuongn09@gmail.com>
2021-06-04 11:09:42 +0700

    add multiple download modal

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ import CreateFolder from './components/Dialog/CreateFolder';
 import UploadFile from './components/Dialog/UploadFile';
 import Lifecycle from './components/Dialog/Lifecycle';
 import ItemDetail from './components/Dialog/ItemDetail';
+import MultipleDownload from './components/Dialog/MultipleDownload';
 
 const App = (props) => {
   const mount = async () => {
@@ -70,7 +71,7 @@ const App = (props) => {
             <Route exact path={paths.BASE_STORAGE} component={StorageLanding} />
             <Route exact path={paths.REGISTER} component={Register} />
             <Route exact path={paths.LOGIN} component={Login} />
-            <Route exact path={paths.TEST} component={ItemDetail} />
+            <Route exact path={paths.TEST} component={MultipleDownload} />
             {/* <Route exact path={paths.TEST} component={Lifecycle} /> */}
             <Route exact path={paths.STORAGE} component={BucketContainer} />
             <Route
---
+++ src/components/Dialog/MultipleDownload.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const MultipleDownload = ({
+  open,
+  onCancel,
+  numOfFiles = 0,
+  totalSize = 0
+}) => {
+  return (
+    <dialog open={true}>
+      <div className="fixed z-10 inset-0 overflow-auto bg-gray-500 bg-opacity-70">
+        <div className="flex flex-col mx-auto mt-12 justify-center max-w-2xl px-4 bg-white shadow rounded-sm my-auto text-gray-600">
+          <h1 className="w-full text-center text-2xl py-6">Download</h1>
+          <hr />
+          <div className="flex flex-row justify-center mt-10 mb-5">
+            <p>You are downloading&nbsp;</p>
+            <p>
+              {numOfFiles} Files {totalSize}
+            </p>
+          </div>
+          <div className="flex flex-row justify-center mt-5 mb-10">
+            <button className="rounded-sm py-2 px-4 mr-2 border border-transparent text-sm font-medium text-white bg-blue-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              I'm Sure. Download Files!
+            </button>
+            <button
+              className="rounded-sm py-2 px-4 border border-transparent text-sm font-medium text-white bg-gray-400 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default MultipleDownload;
