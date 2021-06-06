a58cbfe371ee60b83b03de54540ea4ab393b8b3b
dilvika <cao.d.vi@gmail.com>
2021-06-07 05:03:36 +0700

    add to card

--- src/components/Charts/LineChartCard.js
+++ src/components/Charts/LineChartCard.js
@@ -9,27 +9,54 @@ import {
   Tooltip
 } from 'recharts';
 
-const data = [
-  { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-  { name: 'Page B', uv: 380, pv: 2400, amt: 2400 },
-  { name: 'Page C', uv: 201, pv: 2400, amt: 2400 },
-  { name: 'Page D', uv: 100, pv: 2400, amt: 2400 }
+const months = [
+  undefined,
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
 ];
-const LineChartCard = (props) => {
+
+// uv: value
+const data = [
+  { name: months[7], uv: 600 },
+  { name: months[8], uv: 700 },
+  { name: months[9], uv: 400 },
+  { name: months[10], uv: 300 },
+  { name: months[11], uv: 500 },
+  { name: months[12], uv: 100 },
+  { name: months[1], uv: 700 },
+  { name: months[2], uv: 100 },
+  { name: months[3], uv: 200 },
+  { name: months[4], uv: 300 },
+  { name: months[5], uv: 400 },
+  { name: months[6], uv: 600 }
+];
+const LineChartCard = ({ width = 900, height = 300 }) => {
   return (
-    <LineChart
-      width={600}
-      height={300}
-      data={data}
-      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-    >
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Legend />
-      <Tooltip />
-    </LineChart>
+    <div className="bg-white dark:bg-gray-800 w-auto shadow-lg mx-auto rounded-xl p-4">
+      <LineChart
+        width={width}
+        height={height}
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+
+        <Tooltip />
+      </LineChart>
+    </div>
   );
 };
 
