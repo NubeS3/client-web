fc279b00db8d34605bd6b652f70f975d2dd2f42b
Tuong Nguyen <tuongn09@gmail.com>
2021-06-07 22:45:59 +0700

    add delete bucket and delet files dialog

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ import UploadFile from './components/Dialog/UploadFile';
 import Lifecycle from './components/Dialog/Lifecycle';
 import ItemDetail from './components/Dialog/ItemDetail';
 import MultipleDownload from './components/Dialog/MultipleDownload';
+import DeleteBucket from './components/Dialog/Delete/DeleteFile';
 
 const App = (props) => {
   const mount = async () => {
@@ -71,7 +72,7 @@ const App = (props) => {
             <Route exact path={paths.BASE_STORAGE} component={StorageLanding} />
             <Route exact path={paths.REGISTER} component={Register} />
             <Route exact path={paths.LOGIN} component={Login} />
-            <Route exact path={paths.TEST} component={MultipleDownload} />
+            <Route exact path={paths.TEST} component={DeleteBucket} />
             {/* <Route exact path={paths.TEST} component={Lifecycle} /> */}
             <Route exact path={paths.STORAGE} component={BucketContainer} />
             <Route
---
+++ src/components/Dialog/Delete/DeleteFile.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const DeleteBucket = ({ open, onClose, numOfFiles, totalSize }) => {
+  return (
+    <dialog open={true}>
+      <div className="fixed z-10 inset-0 overflow-auto bg-gray-500 bg-opacity-70">
+        <div className="flex flex-col mx-auto mt-20 justify-center max-w-xl w-auto px-4 bg-white shadow rounded-sm my-auto text-gray-600">
+          <div className="flex flex-col items-center">
+            <div className="flex flex-row w-full">
+              <div className="flex flex-row w-full justify-center py-4">
+                <h1 className="text-2xl">Are you sure?</h1>
+              </div>
+              <button
+                className="self-start my-2 focus:outline-none"
+                onClick={onClose}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={1}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <hr className="w-full" />
+            <div className="flex flex-col my-6 justify-center items-center text-sm">
+              <p>
+                Befor you can delete a bucket, you must first delete all the
+                files in the bucket
+              </p>
+              <div className="h-10" />
+              <div className="col-span-5 flex flex-row justify-center">
+                <button className="rounded-sm py-2 px-4 mr-2 border border-transparent text-sm font-medium text-white bg-red-700 hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-900">
+                  I'm Sure. Delete Bucket!
+                </button>
+                <button
+                  className="rounded-sm py-2 px-4 border border-transparent text-sm font-medium text-white bg-gray-400 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
+                  onClick={onClose}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default DeleteBucket;
